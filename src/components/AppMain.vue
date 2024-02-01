1339590025ad473bc4a70bbd65df5e8d94579ebb
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-01 16:46:21 +0100

    add: api projects request done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,18 @@
 <script>
+import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+      base_url: "http://127.0.0.1:8000/",
+      projects: [],
+    };
+  },
+  created() {
+    axios.get(`${this.base_url}api/projects`).then((resp) => {
+      this.projects = resp.data.results;
+      console.log(this.projects);
+      console.log(resp);
+    });
   },
 };
 </script>
@@ -10,7 +21,11 @@ export default {
   <div class="container">
     <h2>Here you can find all the project</h2>
     <div class="row">
-      <div class="col-4"></div>
+      <div class="col-4">
+        <div v-for="project in projects">
+          <p>{{ project.title }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
