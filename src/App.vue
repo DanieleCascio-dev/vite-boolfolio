ecfed7c425cd90a29b8c84df84d9e6342df789f0
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-01 16:14:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h2>Main page</h2>
+</template>
+
+<style lang="scss">
+@use "../style/general.scss" as *;
+</style>
