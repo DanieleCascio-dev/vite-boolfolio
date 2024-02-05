6b5d71511369a08aae530829ad60383de8263d28
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-05 16:59:51 +0100

    add: pagination (wip)

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -9,24 +9,41 @@ export default {
       projects: [],
       types: [],
       load: false,
+      page: 1,
+      lastPage: 3,
     };
   },
   created() {
     this.load = true;
-    axios.get(`${this.store.baseUrl}api/projects`).then((resp) => {
-      this.projects = resp.data.results;
-      console.log(this.projects);
-    });
-    axios
-      .get(`${this.store.baseUrl}api/types`)
-      .then((resp) => {
-        this.types = resp.data.results;
-        /* console.log(resp);
+    this.getProjects(this.page),
+      axios
+        .get(`${this.store.baseUrl}api/types`)
+        .then((resp) => {
+          this.types = resp.data.results;
+          /* console.log(resp);
       console.log(this.types); */
-      })
-      .finally(() => {
-        this.load = false;
-      });
+        })
+        .finally(() => {
+          this.load = false;
+        });
+  },
+  methods: {
+    getProjects(pageNum) {
+      this.page = pageNum;
+      axios
+        .get(`${this.store.baseUrl}api/projects`, {
+          params: {
+            page: pageNum,
+          },
+        })
+        .then((resp) => {
+          // console.log(resp);
+          this.projects = resp.data.results.data;
+          // console.log(this.projects);
+          this.lastPage = resp.data.results.last_page;
+          // console.log(this.lastPage);
+        });
+    },
   },
   components: { AppCard },
 };
@@ -36,10 +53,19 @@ export default {
   <div class="container">
     <h2>Here you can find all the project</h2>
     <div v-if="load">Loading...</div>
-    <div v-else class="row g-3">
+    <div v-else class="row g-3 mb-3">
       <div class="col-4" v-for="project in projects">
         <AppCard :project="project" :types="types" />
       </div>
+    </div>
+    <!-- Pagination -->
+    <div class="d-flex gap-3">
+      <button class="btn btn-primary" @click="getProjects(this.page - 1)">
+        Prev
+      </button>
+      <button class="btn btn-primary" @click="getProjects(this.page + 1)">
+        Next
+      </button>
     </div>
   </div>
 </template>
